fa77554b61c3671fa5af501163a098016909dbc5
adiltabrez <adil.tabrez@gmail.com>
2020-04-15 14:39:41 +0530

    Delete EmailLicenseInfo.py

--- EmailLicenseInfo.py
+++
@@ -1,8 +0,0 @@
-import smtplib
-
-s = smtplib.SMTP('mail.google.com', 587)
-s.starttls()
-s.login("adil.tabrez@gmail.com", "Glaksyk!@#4567")
-message = "This is a test email from Python."
-s.sendmail("adil.tabrez@gmail.com","adil.tabrez@gmail.com",message)
-s.quit()
